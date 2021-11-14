01c71941d45966f57db60c6b36386981289c2040
lordgent <lorjenx08@gmail.com>
2021-11-15 00:45:59 +0700

    learn oop interface in ts

--- oop2.ts
+++ oop2.ts
@@ -1,20 +1,73 @@
 // Interface
-interface Laptop {
+// interface Laptop {
+//   brand: string;
+//   on(): void;
+//   off(): void;
+// }
+
+// class Thinkpad implements Laptop {
+//   on(): void {
+//     return console.log("Screen On");
+//   }
+//   off(): void {
+//     return console.log("Screen Off");
+//   }
+//   brand: string = "Thinkpad X240";
+// }
+
+// const thinkpad = new Thinkpad();
+// console.log(thinkpad.on());
+
+interface Epayment {
   brand: string;
-  on(): void;
-  off(): void;
+  noreg: number;
+  TakingMoney(): void;
 }
 
-class Thinkpad implements Laptop {
-  on(): void {
-    return console.log("Screen On");
+class Epay implements Epayment {
+  name: string;
+  email: string;
+  password: string;
+  balance: number;
+  address: string;
+  getmoney: number;
+
+  constructor(
+    name: string,
+    email: string,
+    password: string,
+    balance: number,
+    address: string,
+    getmoney: number
+  ) {
+    (this.name = name),
+      (this.email = email),
+      (this.password = password),
+      (this.balance = balance),
+      (this.address = address);
+    this.getmoney = getmoney;
   }
-  off(): void {
-    return console.log("Screen Off");
+
+  brand = "Epayment.com";
+  noreg = 298292039321;
+  TakingMoney(): void {
+    return console.log(
+      ` saldo awal = ${this.balance}  ${
+        this.name
+      } sedang mengambil uang sebesar = ${this.getmoney} total balance = ${
+        this.balance - this.getmoney
+      }`
+    );
   }
-  brand: string = "Thinkpad X240";
 }
 
-const thinkpad = new Thinkpad();
+const userone = new Epay(
+  "lordgent",
+  "lorjenx08@gmail.com",
+  "12345",
+  2400000000,
+  "Surabaya Jawa timur",
+  2000000
+);
 
-console.log(thinkpad.on());
+console.log(userone.TakingMoney());
